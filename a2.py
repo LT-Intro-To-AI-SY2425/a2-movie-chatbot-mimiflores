bb91e759063dcfeda64576fb1e22759d81a88606
Mimi Flores <aflores393@cps.edu>
2024-09-25 10:39:24 -0500

    up to test 7

--- a2.py
+++ a2.py
@@ -21,7 +21,7 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
     # keep checking as long as we haven't hit the end of either pattern or source while
     # pind is still a valid index OR sind is still a valid index (valid index means that
     # the index is != to the length of the list)
-    while pind < len(pattern) and sind < len(source):
+    while pind < len(pattern) or sind < len(source):
         # your job is to fill out the body of this loop
 
         # 1) if we reached the end of the pattern but not source
@@ -32,14 +32,21 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
         # If you get stuck on this one, we encourage you to attempt the other conditions
         #   and come back to this one afterwards
 
+        # elif pattern[pind] == "%":
+        #     result.append(pattern[pind])
+        #     pind += 1
+        #     sind += 1
+
         # 3) if we reached the end of the source but not the pattern
         elif sind == len(source) and pind < len(pattern):
             return None
         # 4) if the current thing in the pattern is an _
-        
+        elif pattern[pind] == "_":
+            result.append(source[sind])
+            pind += 1
+            sind += 1
         # 5) if the current thing in the pattern is the same as the current thing in the
         # source
-
         elif source[sind] == pattern[pind]:
             pind += 1
             sind += 1
